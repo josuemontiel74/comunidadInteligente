d2001a5a51e5651ac1e80c6696d5a50b8151e450
Josue Daniel Montiel <montzbusiness@gmail.com>
2025-09-23 15:11:34 -0500

    Revert "chore: Se crea la carpeta 5_Trimestre"

--- 5_Trimestre/Backend/src/routers/parqueaderos.router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as parqueaderosController from "../controller/parqueaderos.controller.js";
-
-const router = Router();
-
-router.get("/parqueadero", parqueaderosController.mostraParqueaderos);
-router.get("/parqueadero/:id", parqueaderosController.mostraParqueaderosporId);
-export default router;
