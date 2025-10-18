7db63ccee3ec22c359a494c965e2fc15e76aafa5
jhon sebastian serna hurtado <hurtadosernajhonsebastian@gmail.com>
2025-10-17 20:24:26 -0500

    fix(visitas.controller.js): ajustar comentarios y mejorar mensajes de consola en la creación de visita

--- Backend/src/controller/visitas.controller.js
+++ Backend/src/controller/visitas.controller.js
@@ -51,7 +51,7 @@ export const crearVisita = async (req, res) => {
       });
     }
 
-    // 🧍 Crear o actualizar visitante
+    //  Crear o actualizar visitante
     let visitante = await Visitante.findByPk(numeroDocumento);
     if (!visitante) {
       visitante = await Visitante.create({
@@ -135,12 +135,12 @@ export const crearVisita = async (req, res) => {
       observaciones: observaciones || null,
     });
 
-    console.log("✅ Visita creada exitosamente:", visita.toJSON());
+    console.log(" Visita creada exitosamente:", visita.toJSON());
 
     // 🚧 Ocupar parqueadero SOLO si la visita fue creada correctamente
     if (parqueadero && vehiculoMatricula) {
       await parqueadero.update({ estadoId: 3 });
-      console.log(`🅿️ Parqueadero ${codigoParqueadero} ocupado`);
+      console.log(`🅿Parqueadero ${codigoParqueadero} ocupado`);
     }
 
     res.status(201).json({
